24406df7f5bada9e0287835135bd7f1e35c4774f
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-01-22 18:55:40 +0300

    init - profile and course

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,15 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./components/tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'course',
+    loadChildren: () => import('./pages/course/course.module').then( m => m.CoursePageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./modals/profile/profile.module').then( m => m.ProfilePageModule)
   }
+
 ];
 @NgModule({
   imports: [
