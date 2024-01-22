9c74e62144aca1ec933c3e9e8468ac3494e124e6
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-01-22 19:29:17 +0300

    create Course type

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Course } from '../Types/Course';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  getData(): Observable<any[]> {
-    return this.http.get<any[]>(this.dataUrl);
+  getData(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.dataUrl);
   }
 }
