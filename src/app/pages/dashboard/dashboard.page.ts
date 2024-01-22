e444d6d0e7639991924bb3eb0483d90b00171802
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2024-01-22 19:46:21 +0300

    dashboard - init get data

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Course } from 'src/app/Types/Course';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['dashboard.page.scss']
 })
 export class DashboardPage {
+  courseList: Course[] | undefined;
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.dataService.getData().subscribe(
+      {
+        next: (data) => {
+          this.courseList = data;
+          console.log(this.courseList);
+
+        },
+        error: (error) => {
+          console.error('Error loading data', error);
+        }
+      }
+    );
+  }
 
 }
